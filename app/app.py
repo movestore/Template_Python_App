b94419fcfa56f107d4d916482eb23b519adbea68
Iris <idbontekoe@hotmail.com>
2024-11-06 17:28:34 +0100

    Add example App code to template

--- app/app.py
+++ app/app.py
@@ -2,21 +2,68 @@ from sdk.moveapps_spec import hook_impl
 from sdk.moveapps_io import MoveAppsIo
 from movingpandas import TrajectoryCollection
 import logging
+import matplotlib.pyplot as plt
 
+# showcase for importing functions from another .py file (in this case from "./app/getGeoDataFrame.py")
+from app.getGeoDataFrame import get_GDF
+
+# showcase injecting App settings (parameter `year`)
+from dataclasses import dataclass
+@dataclass
+class AppConfig:
+    year: int
 
 class App(object):
 
     def __init__(self, moveapps_io):
         self.moveapps_io = moveapps_io
 
+    # showcase injecting App settings (parameter `year`)
+    @staticmethod
+    def map_config(config: dict):
+        return AppConfig(
+            year=config["year"] if "year" in config else 1960
+        )
+
     @hook_impl
     def execute(self, data: TrajectoryCollection, config: dict) -> TrajectoryCollection:
-        """Your app code goes here"""
+
         logging.info(f'Welcome to the {config}')
 
+        """Your app code goes here"""
+
+        # showcase injecting App settings (parameter `year`)
+        app_config = self.map_config(config=config)
+        data_gdf = get_GDF(data)  # translate the TrajectoryCollection to a GeoDataFrame
+        logging.info(f'Subsetting data for {app_config.year}')
+        # subset the data to only contain the specified year
+        if app_config.year in data_gdf.index.year:
+            result = data_gdf[data_gdf.index.year == app_config.year]
+        else:
+            result = None
+
+        # showcase creating an artifact
+        if result is not None:
+            result.plot(column=data.get_traj_id_col(),alpha=0.5)
+            plot_file = self.moveapps_io.create_artifacts_file("plot.png")
+            plt.savefig(plot_file)
+            logging.info(f'saved plot to {plot_file}')
+        else:
+            logging.warning("Nothing to plot")
+
+        # showcase accessing auxiliary files
         auxiliary_file_a = MoveAppsIo.get_auxiliary_file_path("auxiliary-file-a")
         with open(auxiliary_file_a, 'r') as f:
             logging.info(f.read())
 
-        # return some useful data for next apps in the workflow
-        return data
+        # Translate the result back to a TrajectoryCollection
+        if result is not None:
+            result = TrajectoryCollection(
+                result,
+                traj_id_col=data.get_traj_id_col(),
+                t=data.to_point_gdf().index.name,
+                crs=data.get_crs()
+            )
+
+        # return the resulting data for next Apps in the Workflow
+        return result
--- tests/app/test_app.py
+++ tests/app/test_app.py
@@ -13,6 +13,125 @@ class MyTestCase(unittest.TestCase):
         os.environ['APP_ARTIFACTS_DIR'] = os.path.join(ROOT_DIR, 'tests/resources/output')
         self.sut = App(moveapps_io=MoveAppsIo())
 
+    def test_app_runs(self):
+        # prepare
+        data: mpd.TrajectoryCollection = pd.read_pickle(os.path.join(ROOT_DIR, 'tests/resources/app/input4_LatLon.pickle'))
+        config: dict = {
+            "year": 2014
+        }
+
+        # execute
+        self.sut.execute(data=data, config=config)
+
+    def test_app_config(self):
+        # prepare
+        config = {
+            "year": 2014
+        }
+
+        # execute
+        actual = config
+
+        # verify
+        self.assertEqual(2014, actual["year"])
+
+    def test_app_config_mapping_defaults(self):
+        # prepare
+        config = {}
+
+        # execute
+        actual = self.sut.map_config(config=config)
+
+        # verify
+        self.assertEqual(1960, actual.year)
+
+    def test_year_present(self):
+        # prepare input data
+        df = pd.DataFrame([
+            {'timestamp_utc': "2001-06-11 09:00:00", 'coords_x': 1, 'coords_y': 5, 'track_id': 'ID_1'},
+            {'timestamp_utc': "2001-07-12 09:00:00", 'coords_x': 2, 'coords_y': 4, 'track_id': 'ID_1'},
+            {'timestamp_utc': "2002-08-13 09:00:00", 'coords_x': 3, 'coords_y': 3, 'track_id': 'ID_1'},
+            {'timestamp_utc': "2002-09-14 09:00:00", 'coords_x': 4, 'coords_y': 2, 'track_id': 'ID_1'},
+            {'timestamp_utc': "2002-10-15 09:00:00", 'coords_x': 5, 'coords_y': 1, 'track_id': 'ID_1'},
+            {'timestamp_utc': "2000-06-11 09:00:00", 'coords_x': 1, 'coords_y': 5, 'track_id': 'ID_2'},
+            {'timestamp_utc': "2000-07-12 09:00:00", 'coords_x': 2, 'coords_y': 4, 'track_id': 'ID_2'},
+            {'timestamp_utc': "2001-08-13 09:00:00", 'coords_x': 3, 'coords_y': 3, 'track_id': 'ID_2'},
+            {'timestamp_utc': "2001-09-14 09:00:00", 'coords_x': 4, 'coords_y': 2, 'track_id': 'ID_2'},
+            {'timestamp_utc': "2001-10-15 09:00:00", 'coords_x': 5, 'coords_y': 1, 'track_id': 'ID_2'}
+        ])
+        input = mpd.TrajectoryCollection(
+            df,
+            traj_id_col='track_id',
+            t='timestamp_utc',
+            crs='epsg:4326',
+            x='coords_x', y='coords_y'
+        )
+
+        # prepare configuration
+        config = {
+            "year": 2001
+        }
+
+        # prepare expected data
+        df_e = pd.DataFrame([
+            {'timestamp_utc': "2001-06-11 09:00:00", 'coords_x': 1, 'coords_y': 5, 'track_id': 'ID_1'},
+            {'timestamp_utc': "2001-07-12 09:00:00", 'coords_x': 2, 'coords_y': 4, 'track_id': 'ID_1'},
+            {'timestamp_utc': "2001-08-13 09:00:00", 'coords_x': 3, 'coords_y': 3, 'track_id': 'ID_2'},
+            {'timestamp_utc': "2001-09-14 09:00:00", 'coords_x': 4, 'coords_y': 2, 'track_id': 'ID_2'},
+            {'timestamp_utc': "2001-10-15 09:00:00", 'coords_x': 5, 'coords_y': 1, 'track_id': 'ID_2'}
+        ])
+        expected = mpd.TrajectoryCollection(
+            df_e,
+            traj_id_col='track_id',
+            t='timestamp_utc',
+            crs='epsg:4326',
+            x='coords_x', y='coords_y'
+        )
+
+        # execute
+        actual = self.sut.execute(data=input, config=config)
+
+        # verify timestamps
+        self.assertEqual(actual.to_point_gdf().index.strftime("%Y-%m-%d %H:%M:%S").tolist(),expected.to_point_gdf().index.strftime("%Y-%m-%d %H:%M:%S").tolist())
+
+        # verify track ids
+        self.assertEqual(actual.to_point_gdf()[actual.get_traj_id_col()].unique().tolist(), expected.to_point_gdf()[expected.get_traj_id_col()].unique().tolist())
+
+    def test_year_not_present(self):
+        # prepare input data
+        df = pd.DataFrame([
+            {'timestamp_utc': "2001-06-11 09:00:00", 'coords_x': 1, 'coords_y': 5, 'track_id': 'ID_1'},
+            {'timestamp_utc': "2001-07-12 09:00:00", 'coords_x': 2, 'coords_y': 4, 'track_id': 'ID_1'},
+            {'timestamp_utc': "2002-08-13 09:00:00", 'coords_x': 3, 'coords_y': 3, 'track_id': 'ID_1'},
+            {'timestamp_utc': "2002-09-14 09:00:00", 'coords_x': 4, 'coords_y': 2, 'track_id': 'ID_1'},
+            {'timestamp_utc': "2002-10-15 09:00:00", 'coords_x': 5, 'coords_y': 1, 'track_id': 'ID_1'},
+            {'timestamp_utc': "2000-06-11 09:00:00", 'coords_x': 1, 'coords_y': 5, 'track_id': 'ID_2'},
+            {'timestamp_utc': "2000-07-12 09:00:00", 'coords_x': 2, 'coords_y': 4, 'track_id': 'ID_2'},
+            {'timestamp_utc': "2001-08-13 09:00:00", 'coords_x': 3, 'coords_y': 3, 'track_id': 'ID_2'},
+            {'timestamp_utc': "2001-09-14 09:00:00", 'coords_x': 4, 'coords_y': 2, 'track_id': 'ID_2'},
+            {'timestamp_utc': "2001-10-15 09:00:00", 'coords_x': 5, 'coords_y': 1, 'track_id': 'ID_2'}
+        ])
+        input = mpd.TrajectoryCollection(
+            df,
+            traj_id_col='track_id',
+            t='timestamp_utc',
+            crs='epsg:4326',
+            x='coords_x', y='coords_y'
+        )
+
+        # prepare configuration
+        config = {
+            "year": 2100
+        }
+
+        # execute
+        actual = self.sut.execute(data=input, config=config)
+
+        # verify
+        self.assertIsNone(actual)
+
+    """
+    # Use this test if the App should return the input data
     def test_app_returns_input(self):
         # prepare
         expected: mpd.TrajectoryCollection = pd.read_pickle(os.path.join(ROOT_DIR, 'tests/resources/app/input2_LatLon.pickle'))
@@ -23,3 +142,4 @@ class MyTestCase(unittest.TestCase):
 
         # verif
         self.assertEqual(expected, actual)
+    """
